3b049961870e5bc7d27cb3efdc4e88d795145d78
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2021-10-11 20:10:28 +0000

    Unit1Notes

---
+++ sept22.java
@@ -0,0 +1,27 @@
+/* 
+problem statment
+- identify whats relevant and whats not
+program consume --> output inputs
+prog lang provide tools to solve problems
+librarier prog lang has
+tools to solve problem:
+  current tools, functions and classes
+
+
+
+
+Command line programs
+  java myProgram 4 numbers(doubles)
+  sum: # average: #
+
+write a program that takes 4 decimal #s add them and averages them
+
+STEP 1: Data definition: how are we going to represent the relevant information from the problem
+  -types of information = types of data and how wea re using
+
+data doubles
+  num1 num2 num3 num4
+
+  function signautes functions take sosme
+  function name input something then export something
+*/
