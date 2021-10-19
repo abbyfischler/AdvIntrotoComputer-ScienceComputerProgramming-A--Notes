c59c3c2ddc29b4c309bcc1286cd1f4e37bdfb445
Abby Fischler <abigail.fischler@gmail.com>
2021-10-19 12:11:33 -0700

    updated readME added notes for oct19

---
+++ Unit 1/oct19.java
@@ -0,0 +1,18 @@
+/*
+1. sequence
+2. conditional
+3. iteration
+
+While loops
+While (boolean) is true do a bunch of statements and automaitcally(sequecning part bring you back to top and test expression again) if false go to a new function
+
+*/
+
+class oct19 {
+  public static void main(String[] args) {
+  
+
+  }
+
+
+}
